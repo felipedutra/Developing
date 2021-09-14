363aadf3e1156d01a2ea21cb5b4e5ef37c3a39b7
Felipe Dutra <felipedutrateixeira@gmail.com>
2021-09-13 23:07:58 -0300

    Removing 3rd party contents

--- 42_ecole/shell01/ex01/print_groups.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-groups $FT_USER | sed -e "s: :,:g" | tr -d "\n"
--- Web/push.sh
+++ Web/push.sh
@@ -1,3 +1,3 @@
-#rsync -avP * felipedutra@172.104.20.105:~/.html/
+rsync -avP * felipedutra@172.104.10.37:~/html/
 #ssh felipedutra@192.168.1.15
-rsync -avP * felipedutra@192.168.1.15:~/.html/
+rsync -avP * felipedutra@192.168.1.15:~/html/
