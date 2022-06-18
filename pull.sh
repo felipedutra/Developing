5ea903e1b5311d28dbf64f1d5140b7a39b5b1d9b
Felipe Dutra <felipedutrateixeira@gmail.com>
2022-06-18 12:57:19 -0300

    Auto Commit

--- pull.sh
+++ pull.sh
@@ -1,1 +1,6 @@
 git clone https://github.com/csev/py4e.git ../py4e
+git clone git@github.com:felipedutra/Map3121.git ../Map3121
+git clone git@github.com:felipedutra/onedrive.git ../onedrive
+git clone git@github.com:felipedutra/StudyLink.git ../StudyLink
+git clone git@github.com:felipedutra/42_Subjects.git ../42_Subjects
+git clone git@github.com:felipedutra/atom.git ../atom
