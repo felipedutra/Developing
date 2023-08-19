c1d545850820c3b1018e293904d151166a247713
Felipe Dutra <felipe.teixeira@datagro.com>
2023-08-19 15:44:01 -0300

    VHDL aula 2

--- VHDL/dockerVhdl/docker.sh
+++ VHDL/dockerVhdl/docker.sh
@@ -1,5 +1,5 @@
 docker pull ghdl/ghdl:ubuntu18-mcode
-docker run --name ghdl --rm  -u $(id -u):$(id -g) -v ~/Developing/Developing/VHDL/:/workspace -w /workspace -it ghdl/ghdl:ubuntu18-mcode  /bin/bash
+docker run --name ghdl --rm  -u $(id -u):$(id -g) -v C:/Users/fteixeira/Programming/Developing/VHDL/:/workspace -w /workspace -it ghdl/ghdl:ubuntu18-mcode  /bin/bash
 
 
 
