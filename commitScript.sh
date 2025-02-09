70fb0ebeb9ea2a4836bf934e6776e95b0a1b2f57
Felipe Dutra <felipedutrateixeira@gmail.com>
2025-02-08 21:01:41 -0300

    Auto Commit

--- commitScript.sh
+++ commitScript.sh
@@ -2,3 +2,6 @@ find . -type f  -name ".DS_Store*" -print -delete
 git config --global user.email "felipedutrateixeira@gmail.com"
 git config --global user.name "Felipe Dutra"
 git add .
+git commit -m "Auto Commit"
+git push
+s
